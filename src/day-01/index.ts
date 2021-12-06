8092ff67ff8f368c9f40d531e8bb3dc2bab014f5
Jeff Leverenz <jeff.leverenz@gmail.com>
2021-12-06 12:31:31 -0600

    Initial solution for day-01

---
+++ src/day-01/index.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import * as fs from 'fs';
+
+function readFile(filename: string): number[] {
+  var array = fs.readFileSync(filename).toString().trim().split("\n");
+  return array.map((i: string) => parseInt(i));
+}
+
+function calculateDescents(depthArray: number[]): number {
+  let count = 0;
+  let last = depthArray[0];
+  depthArray.slice(1).forEach((i: any) => {
+    count += i > last ? 1 : 0;
+    last = i;
+  })
+  return count;
+}
+
+describe('day-01', () => {
+  it('sample', () => {
+    const answer = calculateDescents(readFile('src/day-01/sample'));
+    expect(answer).to.equal(7);
+  });
+
+  it('input', () => {
+    const answer = calculateDescents(readFile('src/day-01/input'));
+    expect(answer).to.equal(1477);
+  });
+});
