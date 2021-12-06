103385e872bb2f5b273281459886406217fdc279
Jeff Leverenz <jeff.leverenz@gmail.com>
2021-12-06 13:12:47 -0600

    Simplify Board type to BoardCombos

--- src/day-04/part-2.ts
+++ src/day-04/part-2.ts
@@ -1,17 +1,15 @@
 import { expect } from 'chai';
 import { readFile, testSetup } from '../util';
 
-interface Board {
-  sets: Set<number>[];
-}
+type BoardCombos = Set<number>[];
 
-function buildBoards(input: string[]): Board[] {
+function buildBoards(input: string[]): BoardCombos[] {
   const boardRows = input.filter(i => i != '');
   const boards = Array.from(Array(boardRows.length / 5).keys()).map(i => {
     const rangeOf5 = Array.from(Array(5).keys());
     const rows = rangeOf5.map(r => boardRows[i * 5 + r].trim().split(/ +/).map(n => parseInt(n)));
     const cols = rangeOf5.map(r => rows.map(row => row[r]));
-    return { sets: [...rows, ...cols].map(i => new Set(i)) };
+    return [...rows, ...cols].map(i => new Set(i));
   });
   return boards;
 }
@@ -20,23 +18,21 @@ function buildDraws(inputDraws: string): number[] {
   return inputDraws.trim().split(/,/).map(i => parseInt(i));
 }
 
-function calculateFinalScore(boards: Board[], draws: number[]) {
+function calculateFinalScore(boards: BoardCombos[], draws: number[]) {
   for(let i = 0; i < draws.length; i++) {
     const draw = draws[i];
 
     // update boards
     boards.forEach(board => {
-      board.sets.forEach(set => {
-        set.delete(draw);
-      });
+      board.forEach(s => s.delete(draw));
     });
 
     // find the last winning board
-    const boardIsWinner = (board: Board) => board.sets.filter(i => i.size == 0).length > 0;
+    const boardIsWinner = (board: BoardCombos) => board.filter(i => i.size == 0).length > 0;
     const nonWinningBoards = boards.filter(i => !boardIsWinner(i));
     if(nonWinningBoards.length == 0) {
       const winningBoard = boards[0];
-      const unmarked = winningBoard.sets.reduce((a,i) => new Set([...a, ...i]));
+      const unmarked = winningBoard.reduce((a,i) => new Set([...a, ...i]));
       const sumUnmarked = [...unmarked].reduce((a,i) => a + i);
       return sumUnmarked * draw;
     }
