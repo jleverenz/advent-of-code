ac3f6200f4fd8ed7b8e69f5913abc6d06e167dc1
Jeff Leverenz <jeff.leverenz@gmail.com>
2021-12-10 04:29:55 -0600

    Refactor makeRange

--- src/day-05/common.ts
+++ src/day-05/common.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import { makeRange } from '../util';
 
 type Line = { start: number[]; end: number[] };
 
@@ -49,31 +49,3 @@ function sortCoordinates(line: Line): Line {
   });
   return { start: incLine[0], end: incLine[1] };
 }
-
-function makeRange(p1: number, p2?: number) {
-  if (p2 === undefined) {
-    return Array.from(Array(p1).keys());
-  } else {
-    if (p2 >= p1) {
-      return Array.from(Array(p2 - p1 + 1).keys()).map((i) => p1 + i);
-    } else {
-      return Array.from(Array(p1 - p2 + 1).keys()).map((i) => p1 - i);
-    }
-  }
-}
-
-describe('common', () => {
-  it('makeRange with one param', () => {
-    expect(makeRange(0)).to.deep.equal([]);
-    expect(makeRange(1)).to.deep.equal([0]);
-    expect(makeRange(5)).to.deep.equal([0, 1, 2, 3, 4]);
-  });
-
-  it('makeRange with two params', () => {
-    expect(makeRange(0, 3)).to.deep.equal([0, 1, 2, 3]);
-    expect(makeRange(3, 0)).to.deep.equal([3, 2, 1, 0]);
-    expect(makeRange(1, 3)).to.deep.equal([1, 2, 3]);
-    expect(makeRange(1, 1)).to.deep.equal([1]);
-    expect(makeRange(2, -2)).to.deep.equal([2, 1, 0, -1, -2]);
-  });
-});
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,4 @@
+import { expect } from 'chai';
 import * as fs from 'fs';
 
 export function readFile(filename: string): string[] {
@@ -12,3 +13,31 @@ export function testSetup(dayString: string) {
     process.chdir('../..');
   });
 }
+
+export function makeRange(p1: number, p2?: number) {
+  if (p2 === undefined) {
+    return Array.from(Array(p1).keys());
+  } else {
+    if (p2 >= p1) {
+      return Array.from(Array(p2 - p1 + 1).keys()).map((i) => p1 + i);
+    } else {
+      return Array.from(Array(p1 - p2 + 1).keys()).map((i) => p1 - i);
+    }
+  }
+}
+
+describe('util', () => {
+  it('makeRange with one param', () => {
+    expect(makeRange(0)).to.deep.equal([]);
+    expect(makeRange(1)).to.deep.equal([0]);
+    expect(makeRange(5)).to.deep.equal([0, 1, 2, 3, 4]);
+  });
+
+  it('makeRange with two params', () => {
+    expect(makeRange(0, 3)).to.deep.equal([0, 1, 2, 3]);
+    expect(makeRange(3, 0)).to.deep.equal([3, 2, 1, 0]);
+    expect(makeRange(1, 3)).to.deep.equal([1, 2, 3]);
+    expect(makeRange(1, 1)).to.deep.equal([1]);
+    expect(makeRange(2, -2)).to.deep.equal([2, 1, 0, -1, -2]);
+  });
+});
